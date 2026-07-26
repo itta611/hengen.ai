c8191c6b0f7e1e331ba937551ec4b2d706c1b2c1
Itta Funahashi <itta@itta.dev>
2026-07-26 22:45:30 +0900

    feat: リンクにする

--- apps/web/components/landing/header.tsx
+++ apps/web/components/landing/header.tsx
@@ -3,6 +3,7 @@
 import { Logo } from "@/components/logo"
 import { Button } from "@/components/ui/button"
 import { useAuthDialog } from "@/hooks/use-auth-dialog"
+import Link from "next/link"
 
 export function LandingHeader() {
   const { openAuthDialog } = useAuthDialog()
@@ -10,7 +11,9 @@ export function LandingHeader() {
   return (
     <nav className="px-5">
       <div className="flex items-center justify-between py-4 mx-auto max-w-300">
-        <Logo height={30} />
+        <Link href="/">
+          <Logo height={30} />
+        </Link>
         {/* <div className="hidden items-center gap-8 md:flex">
           <a href="#workflow">使い方</a>
           <a href="#advantages">特長</a>
